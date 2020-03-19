8c1cc9f632f89e64c6ad1ce027924ff6730caf0c
Heasson1337 <62406835+Heasson1337@users.noreply.github.com>
2020-03-20 00:06:04 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,74 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log("Streamstatus by Heasson");
+
+  client.user
+    .setActivity(
+      `🇩​🇪​🇻​🇪​🇱​🇴​🇵​🇵​🇪​🇩​ 🇧​🇾​ 🇭​🇪​🇦​🇸​🇸​🇴​🇳​`,
+      {
+        type: "STREAMING",
+        url: "https://www.twitch.tv/heasson1337"
+      }
+    )
+    .then(presence =>
+      console.log(
+        `Your Status has been set to  ${
+          presence.game ? presence.game.none : "none"
+        }`
+      )
+    )
+    .catch(console.error);
+});
+
+client.login("Njg5ODk5MDkzNTE4MDU3NDc3.XnP1ag.U-XuIZqK4tS4-T_49f65HyI9-ZU");
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log("Streamstatus by Heasson");
+
+  client.user
+    .setActivity(
+      `🇩​🇪​🇻​🇪​🇱​🇴​🇵​🇵​🇪​🇩​ 🇧​🇾​ 🇭​🇪​🇦​🇸​🇸​🇴​🇳​`,
+      {
+        type: "STREAMING",
+        url: "https://www.twitch.tv/heasson1337"
+      }
+    )
+    .then(presence =>
+      console.log(
+        `Your Status has been set to  ${
+          presence.game ? presence.game.none : "none"
+        }`
+      )
+    )
+    .catch(console.error);
+});
+
+client.login("Njg5OTAzNTQ5MTYwNzUxMTM3.XnP0Yw.qNolfGu2TOxYzcPy_ISYQMMi7IY");
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log("Streamstatus by Heasson1337");
+
+  client.user
+    .setActivity(
+      `🇩​🇪​🇻​🇪​🇱​🇴​🇵​🇵​🇪​🇩​ 🇧​🇾​ 🇭​🇪​🇦​🇸​🇸​🇴​🇳​`,
+      {
+        type: "STREAMING",
+        url: "https://www.twitch.tv/heasson1337"
+      }
+    )
+    .then(presence =>
+      console.log(
+        `Your Status has been set to  ${
+          presence.game ? presence.game.none : "none"
+        }`
+      )
+    )
+    .catch(console.error);
+});
+
+client.login("NTAzNjcwODI2NDk5OTY0OTI5.XmFy8g.wp8LAkiKruDjfn41V_gXVPfduoI");
